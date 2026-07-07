90f85bf7265346751d784ed4de7e33c5bc7c4f8a
UeharaRyuichiro1108 <jinzhikamui@gmail.com>
2026-07-07 16:08:52 +0900

    first character site

---
+++ src/data/characters.js
@@ -0,0 +1,22 @@
+export const characters = [
+  {
+    id: "ao",
+    name: "白峰 アオ",
+    group: "星灯機関",
+    image: "/images/characters/ao.png",
+    catchcopy: "星を読む観測者",
+    description:
+      "星灯機関に所属する観測者。静かだが、異変の兆候にだけ異常に敏感。",
+    tags: ["ao", "main"],
+  },
+  {
+    id: "kuro",
+    name: "黒瀬 クロ",
+    group: "月蝕同盟",
+    image: "/images/characters/kuro.png",
+    catchcopy: "夜を連れて歩く少年",
+    description:
+      "月蝕同盟に関わる謎の少年。アオとは過去に何らかの関係がある。",
+    tags: ["kuro", "main"],
+  },
+];
---
+++ src/data/illustrations.js
@@ -0,0 +1,14 @@
+export const illustrations = [
+  {
+    id: "observatory",
+    title: "夜明けの観測室",
+    image: "/images/illustrations/observatory.png",
+    tags: ["ao", "kuro"],
+  },
+  {
+    id: "blue-star",
+    title: "青い星の記録",
+    image: "/images/illustrations/blue-star.png",
+    tags: ["ao"],
+  },
+];
